9d1585552715e0bb63af0f5d9391f7a96976350f
jana <janarthan.paramanandam@ge.com>
2018-10-27 22:34:20 +0530

    Add splitIntoSentences and tokenize

--- NamedEntityRecognizer/src/__init__.py
+++ NamedEntityRecognizer/src/__init__.py
@@ -1,10 +1,29 @@
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 
-def method1():
-    print ("printing this line in console")
+
+def splitIntoSentences(text):
+    print ("splitIntoSentences, text \n", text)
+    trimmedText = text.strip(' \n')
+    sentences = sent_tokenize(trimmedText)
+    print ("len", len(sentences))
+    print (sentences)
+    return sentences
 
 
 def main():
-    method1()
+    sentences = splitIntoSentences("""
+    this is a simple sentence. this is a simple sentence1. 
+    this is a simple sentence3
+    """)
+
+    for sent in sentences:
+        words = word_tokenize(sent)
+        print (words)
+
+    # method1("""
+    #     this's a sent tokenize test.
+    #     """)
 
 
 main()
